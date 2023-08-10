acf5b690265aad26ce612a7ea247aa21c35286ca
Melodycloudu <51493662+Melodycloudu@users.noreply.github.com>
2023-08-10 22:13:06 +0100

    Update play.sh

--- play.sh
+++ play.sh
@@ -3,3 +3,6 @@ if [ ! -f "runtime/envs/koboldai/bin/python" ]; then
 ./install_requirements.sh cuda
 fi
 bin/micromamba run -r runtime -n koboldai python aiserver.py $*
+from pyngrok import ngrok
+public_url = ngrok.connect(5000)
+print(public_url)
